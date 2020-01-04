66f50a98270ba40112e1438d7ee31b939fd156f5
moroball14 <daiki.mr.0330@gmail.com>
2020-01-04 15:57:40 +0900

    router-linkの設定

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/users">Users</router-link>
+    </nav>
     <router-view></router-view>    
   </div>
 </template>
